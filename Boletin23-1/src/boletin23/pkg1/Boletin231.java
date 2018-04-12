b522472a1ed36f128eea063a351799b037579b92
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-04-12 12:39:44 +0200

    Commit git.

---
+++ Boletin23-1/src/boletin23/pkg1/Boletin231.java
@@ -0,0 +1,17 @@
+
+package boletin23.pkg1;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin231 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Eventos obxEventos=new Eventos();
+    }
+    
+}
